b62d52fdf6045ff3e32c8aa907c093b4ad999f03
Pete <petehorn6@gmail.com>
2021-10-08 23:04:31 +0100

    wierd errors

---
+++ tariff_functions/GetDailyAvg.py
@@ -0,0 +1,4 @@
+import json
+
+def calcAvg (PeriodValues):
+    return 3.2
---
+++ tests/test_daily_avg.py
@@ -0,0 +1,8 @@
+from tariff_functions import GetDailyAvg
+
+result = GetDailyAvg("blah")
+
+if (result == 3.5):
+    assert True
+else:
+    assert False

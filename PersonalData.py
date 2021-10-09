75cf2b37ff25834601a44deabd34c3b9fa819e21
Pete <petehorn6@gmail.com>
2021-10-09 23:53:08 +0100

    added personal data

---
+++ PersonalData.py
@@ -0,0 +1,10 @@
+dict = {
+    'HomeInfluxDBName': 'home',
+    'TestInfluxDBName': 'sandbox',
+    'InfluxDB_IP': '192.168.68.109',
+    'InfluxDB_user': 'pete',
+    'InfluxDB_pw': 'AlfieJames1'
+}
+
+def getValues():
+    return dict

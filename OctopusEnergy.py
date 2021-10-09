98f55b45d6cd9dd22920ee809f160b415f02da0f
Pete <petehorn6@gmail.com>
2021-10-09 22:07:35 +0100

    added the curl command to the Octopus Energy API py file

--- OctopusEnergy.py
+++ OctopusEnergy.py
@@ -1,3 +1,6 @@
+import requests
+import json
+
 URLs = {
     'test': 'testURL',
     'tariff': 'products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-H/standard-unit-rates'
@@ -5,3 +8,11 @@ URLs = {
 
 def createURL (date, type):
     return 'https://api.octopus.energy/v1/' + URLs[type] + '/?period_from=' + date + 'T00:00:00Z&period_to=' + date + 'T23:59:00Z'
+
+def ReturnEnergyDataStr(date):
+    APIKEY = 'sk_live_al6fhRh540BSzKu16jjIkEKE'
+    req = requests.get(createURL(date), auth=(APIKEY,''))
+    rawText = req.text
+    jsonText = json.loads(rawText)
+    results = jsonText["results"]
+    return results

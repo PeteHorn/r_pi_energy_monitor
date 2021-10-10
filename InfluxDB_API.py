c5b2e0b0af6b503001660c4f642dfc3121c9bfa2
Pete <petehorn6@gmail.com>
2021-10-10 20:20:11 +0100

    adding the read to influxdb write (mainly for testing at the moment)

--- InfluxDB_API.py
+++ InfluxDB_API.py
@@ -10,4 +10,7 @@ port = 8086
 def WriteData(dbname, json_body):
     client = InfluxDBClient(host=IP, port=port, username=user, password=pw, database=dbname)
     client.write_points(json_body)
+    result = client.query('select value from cpu_load_short;')
+    print("Result: {0}".format(result))
     client.close()
+

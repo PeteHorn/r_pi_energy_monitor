ba0b132b617b093ac85e1c6b4b1d0000bbe9c417
Pete <petehorn6@gmail.com>
2021-10-18 22:04:56 +0100

    maybe that will work

--- MQTT.py
+++ MQTT.py
@@ -1,7 +1,10 @@
 import paho.mqtt.client as mqtt
 import PersonalData
 
+def DailyUpdate(topic, data):
+    client.publish(topic, payload=data, qos=0, retain=False)
+
 client = mqtt.Client()
 client.connect(PersonalData.getValues()['MQTT_IP'], 1883, 60)
-client.publish('test_topic', payload='blah', qos=0, retain=False)
+DailyUpdate('test_topic', 'blah blah blah')
 client.disconnect()

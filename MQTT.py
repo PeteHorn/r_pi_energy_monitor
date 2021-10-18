97ffdac70600935c577579a86a2dfff96b9ad72b
Pete <petehorn6@gmail.com>
2021-10-18 22:13:02 +0100

    all in one function

--- MQTT.py
+++ MQTT.py
@@ -2,8 +2,7 @@ import paho.mqtt.client as mqtt
 import PersonalData
 
 def DailyUpdate(topic, data):
+    client = mqtt.Client()
+    client.connect(PersonalData.getValues()['MQTT_IP'], 1883, 60)
     client.publish(topic, payload=data, qos=0, retain=False)
-
-client = mqtt.Client()
-client.connect(PersonalData.getValues()['MQTT_IP'], 1883, 60)
-client.disconnect()
+    client.disconnect()

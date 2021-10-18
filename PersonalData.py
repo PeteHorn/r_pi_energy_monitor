1ba49fba34bf917bcaae28ca84bdfb83ca07c266
Pete <petehorn6@gmail.com>
2021-10-18 21:49:44 +0100

    more stupid syntax

--- PersonalData.py
+++ PersonalData.py
@@ -3,8 +3,8 @@ dict = {
     'TestInfluxDBName': 'sandbox',
     'InfluxDB_IP': '192.168.68.109',
     'InfluxDB_user': 'pete',
-    'InfluxDB_pw': 'AlfieJames1'
-    'MQTT_IP': '192.168.68.109'
+    'InfluxDB_pw': 'AlfieJames1',
+    'MQTT_IP': '192.168.68.109',
     'OctopusAPIkey': 'sk_live_al6fhRh540BSzKu16jjIkEKE'
 }
 

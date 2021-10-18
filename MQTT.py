025a6e14fbf8177fdf1b9ca3d80f4ed1bc75e897
Pete <petehorn6@gmail.com>
2021-10-18 21:58:33 +0100

    remove the loop forever

--- MQTT.py
+++ MQTT.py
@@ -7,10 +7,4 @@ client = mqtt.Client()
 print(PD['MQTT_IP'])
 client.connect(PD['MQTT_IP'], 1883, 60)
 client.publish('test_topic', payload='blah', qos=0, retain=False)
-
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+client.disconnect()

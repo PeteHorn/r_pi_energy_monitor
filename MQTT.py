9ff8d1c564b555991aff22ca6062c92213e30c50
Pete <petehorn6@gmail.com>
2021-10-18 23:14:14 +0100

    trying to avoid naming conflicts

--- MQTT.py
+++ MQTT.py
@@ -3,9 +3,9 @@ import PersonalData
 import json
 
 def DailyUpdate(data):
-    client = mqtt.Client()
-    client.connect(PersonalData.getValues()['MQTT_IP'], 1883, 60)
+    MQTT_client = mqtt.Client()
+    MQTT_client.connect(PersonalData.getValues()['MQTT_IP'], 1883, 60)
     j = json.loads(data)
     for element in j:
-        client.publish(element['topic'], payload=element['data'], qos=0, retain=False)
-    client.disconnect()
+        MQTT_client.publish(element['topic'], payload=element['data'], qos=0, retain=False)
+    MQTT_client.disconnect()

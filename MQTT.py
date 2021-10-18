375e24dbb9a532d66ccbf4a02ffad89dc661eea7
Pete <petehorn6@gmail.com>
2021-10-18 21:52:47 +0100

    blah

--- MQTT.py
+++ MQTT.py
@@ -1,10 +1,11 @@
 import paho.mqtt.client as mqtt
 import PersonalData
 
+PD = PersonalData.getValues
 
 client = mqtt.Client()
 
-client.connect(PersonalData.getValues['MQTT_IP'], 1883, 60)
+client.connect(PD['MQTT_IP'], 1883, 60)
 client.publish('test_topic', payload='blah', qos=0, retain=False)
 
 

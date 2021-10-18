2ed3de03d74b9048c3c2ffb77b65b20615c139e8
Pete <petehorn6@gmail.com>
2021-10-18 22:54:20 +0100

    uyguyg

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -71,7 +71,7 @@ def test_MQTT_publishing():
     testdata = []
     testdata.append({
         'topic': 'test_topic',
-        'data': datetime.datetime.now.strftime("%H:%M:%S")
+        'data': datetime.datetime.now()
     })
     testJSON = json.dumps(testdata)
     MQTT.DailyUpdate(testJSON)

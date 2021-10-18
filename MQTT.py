9386c6931efbe91ca8852e1a7ebaf138795e9c5f
Pete <petehorn6@gmail.com>
2021-10-18 21:55:46 +0100

    testing

--- MQTT.py
+++ MQTT.py
@@ -4,7 +4,7 @@ import PersonalData
 PD = PersonalData.getValues
 
 client = mqtt.Client()
-
+print(PD['MQTT_IP'])
 client.connect(PD['MQTT_IP'], 1883, 60)
 client.publish('test_topic', payload='blah', qos=0, retain=False)
 
